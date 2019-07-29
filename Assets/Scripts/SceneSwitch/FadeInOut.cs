9aab416ba6f44ddde53d3414ba17335c9b98214f
Junchen Cai <caleden@163.com>
2019-07-29 18:58:05 +0800

    fade（sceneswitch）
    淡入淡出

---
+++ Assets/Scripts/SceneSwitch/FadeInOut.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;//注意添加RawImage命名空间
+
+public class FadeInOut : MonoBehaviour
+{
+    public bool isBlack = false;
+    public float fadeSpeed = 2f;
+    public RawImage rawImage;
+    public RectTransform rectTransform;
+
+    void Start()
+    {
+        rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);//使背景满屏
+        rawImage.color = Color.black;
+    }
+
+    void Update()
+    {
+        if (isBlack == false)
+        {
+            rawImage.color = Color.Lerp(rawImage.color, Color.clear, Time.deltaTime * fadeSpeed);//渐亮
+            if (rawImage.color.a < 0.05f)
+            {
+                rawImage.color = Color.clear;
+            }
+        }
+        else if (isBlack)
+        {
+            rawImage.color = Color.Lerp(rawImage.color, Color.black, Time.deltaTime * fadeSpeed);//渐暗
+            if (rawImage.color.a > 0.95f)
+            {
+                rawImage.color = Color.black;
+                isBlack = false;
+            }
+        }
+    }
+
+    //切换状态
+    public void BackGroundControl(bool b)
+    {
+        if (b == true)
+            isBlack = true;
+        else
+            isBlack = false;
+    }
+}
+
