6cd64e1595f257926ec226f1b558eea49bbc36dc
Junchen Cai <caleden@163.com>
2019-07-31 21:41:10 +0800

    stupid start cg
    click to continue

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,6 +15,13 @@ public class GameManager : MonoBehaviour
     public Prefabs GamePrefabs;
     public Sounds GameSounds;
     public Eyecatchs GameEyecatchs;
+
+    //CG Setting
+    public Image startCg;
+    public int id_nowCg = 0;
+    private int num_startCg = 5;
+    public bool isStartCG = false;
+
     public LayerMask CharacterLayer;
     public LayerMask EnemyLayer;
     public LayerMask ProjectileLayer;
@@ -33,7 +41,7 @@ public class GameManager : MonoBehaviour
     private HelpMessageStats helpMessageStats;
 
     public bool isAtNight;
-
+    
     public bool eyecatchShowing = false;
     public bool isEyecatching = false;
     public FadeInOut m_Fade;
@@ -52,6 +60,7 @@ public class GameManager : MonoBehaviour
         isAtNight = false;
         IsSwitchShowed = false;
         this.MapGenerator = base.GetComponent<NewMapGenerator>();
+        startCg.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
         info = GameObject.Find("/Canvas/PlayerInfo").GetComponent<PlayerInfo>();
     }
 
@@ -262,6 +271,38 @@ public class GameManager : MonoBehaviour
         this.GameMenus.StageFailedMenu.SetActive(true);
         this.FadeBlackBars(true);
     }
+    
+    /*
+    public void CGPlay()
+    {
+        startCg.gameObject.SetActive(true);
+        for (int i=1;i<=num_startCg;i++)
+        {
+            id_nowCg = i;
+            string path = "Sprites/startCG/" + i.ToString();
+            Sprite spriteNow = Resources.Load(path, typeof(Sprite)) as Sprite;
+            Debug.Log(spriteNow);
+            startCg.GetComponent<Image>().sprite = spriteNow;
+            Thread.Sleep(2000);
+            m_Fade.BackGroundControl(false);
+        }
+        //isStartCG = false;
+        //startCg.gameObject.SetActive(false);
+    }
+    */
+
+    public void NextCG()
+    {
+        if (id_nowCg < 0)
+            return;
+        if(id_nowCg<num_startCg)
+        {
+            id_nowCg++;
+            m_Fade.BackGroundControl(false);
+            string path = "Sprites/startCG/" + id_nowCg.ToString();
+            startCg.GetComponent<Image>().sprite = Resources.Load(path, typeof(Sprite)) as Sprite;
+        }
+    }
 
     private void Start()
     {
@@ -270,6 +311,8 @@ public class GameManager : MonoBehaviour
         this.GameMenus.BottomBar.canvasRenderer.SetAlpha(0f);
         this.GameMenus.BackBar.canvasRenderer.SetAlpha(0f);
         this.stageFailed = true;
+        isStartCG = true;
+        
     }
 
     public void TogglePause()
@@ -282,7 +325,27 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if (isEyecatching)
+        if (isStartCG)
+        {
+            if (id_nowCg == 0) 
+            {
+                startCg.gameObject.SetActive(true);
+                NextCG();
+            }
+            if (Input.GetButtonDown("Start"))
+            {
+                if(id_nowCg<num_startCg)
+                {
+                    NextCG();
+                }
+                else
+                {
+                    isStartCG = false;
+                    startCg.gameObject.SetActive(false);
+                }
+            }
+        }
+        else if (isEyecatching)
         {
             if (!eyecatchShowing)
             {
@@ -359,7 +422,6 @@ public class GameManager : MonoBehaviour
         public GameObject StageClearMenu;
         public GameObject StageFailedMenu;
         public Text GameStats;
-
         public GameObject MessagePanel;
     }
 
@@ -400,5 +462,7 @@ public class GameManager : MonoBehaviour
         public GameObject Background;
         public GameObject Eyecatch1;
     }
+
+
 
 }
